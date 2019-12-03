08f8f923a789568b83fc71ae0068c449252350a8
Mac Decker <mcdecke@gmail.com>
2019-12-03 15:51:43 -0700

    first go

--- driver.cpp
+++ driver.cpp
@@ -1,76 +1,72 @@
 // Driver program
 #include<iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
 #include "hash.hpp"
 using namespace std;
-
-// GOLD TODO Complete this function
-void printPairs(int* arr, int size, int sum, HashTable ht){
-
-  bool pairFound = false;
-  //TODO :
-  // iterate over each element in the array,
-  for (int i = 0; i < size; i++) {
-    node* x = ht.searchItem(sum-arr[i]);
-
-    if (x == nullptr) {
-      break;
-    }
-
-    cout << x->key ;
-    while (x->next != nullptr) {
-      cout << ", " << x->next->key;
-      x = x->next;
-    }
-    cout << endl;
-    pairFound = true;
-  }
-  // search for sum-arr[i] in the hashtable, if you find it, print out the pairs giving this sum
-  // and you are done!
-  if(pairFound==false){
-    cout<<"pair not found";
-  }
-
-  return;
-
-}
 
 
 
 int main()
 {
-    int a[] = {15, 11, 27, 8, 12, 7, 18};
-    int n = 7;
 
-    // 7 is count of buckets in hash table
-    HashTable ht(7);
+  //set up tables
+  int TABLE_SIZE = 10009;
+  int pre[TABLE_SIZE];
+  int post[TABLE_SIZE];
 
-    // insert the keys into the hash table.
-    // SILVER TODO : Complete insertItem() function
-    for (int i = 0; i < n; i++) {
-        ht.insertItem(a[i]);
-    }
+  HashTable ht(TABLE_SIZE);
 
-    cout<< "Contents of the hash table are"<<endl;
-    // SILVER TODO : Complete printTable() function
-    ht.printTable();
+    string filename = "";
+  	if (argc < 2) {
+  		cout << "Not enough arguments!\n";
+  	} else if (argc > 2) {
+  		cout << "Too many arguments\n";
+  	} else {
+  		filename = argv[1];
+  	}
 
-    cout<<endl;
+  	ifstream fs; // input stream OBJECT
+  	fs.open(filename);
 
-    int searchFor = 9;
-    // SILVER TODO : Complete searchItem() function
-    if(ht.searchItem(searchFor))
-     {
-       cout<<searchFor<<" found in the hash table"<<endl;
-     }
-    else{
-      cout<<searchFor<<" not found in the hash table"<<endl;
-    }
-    cout<<"------------------------------------"<<endl;
+    //
+    // // 10009 is count of buckets in hash table
 
-    int sum = 19;
+    int number;
+    int counter = 0;
 
-    // GOLD TODO Complete printPairs() function
-    printPairs(a,n,sum,ht);
+    	while (getline(fs, number, ',')) {
+        cout << counter << " + " << number << endl;
+        counter++;
+    //
+    // // insert the keys into the hash table.
+
+    // for (int i = 0; i < n; i++) {
+    //     ht.insertItem(a[i]);
+    // }
+    //
+    // cout<< "Contents of the hash table are"<<endl;
+    // // SILVER TODO : Complete printTable() function
+    // ht.printTable();
+    //
+    // cout<<endl;
+    //
+    // int searchFor = 9;
+    // // SILVER TODO : Complete searchItem() function
+    // if(ht.searchItem(searchFor))
+    //  {
+    //    cout<<searchFor<<" found in the hash table"<<endl;
+    //  }
+    // else{
+    //   cout<<searchFor<<" not found in the hash table"<<endl;
+    // }
+    // cout<<"------------------------------------"<<endl;
+    //
+    // int sum = 19;
+    //
+    // // GOLD TODO Complete printPairs() function
+    // printPairs(a,n,sum,ht);
 
     return 0;
 }
